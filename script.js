eab6ebb81ef602bc638746298a7cb36fecba2e45
peterwakoko <peterwakoko@gmail.com>
2023-03-07 00:34:21 +0300

    client validation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var user_name = document.getElementById("user_name");
 var user_password = document.getElementById("user_password");
+var login_btn = document.getElementById("login_btn");
 
 function validateUserName() {
   var user_name_value = document.getElementById("user_name").value;
@@ -19,5 +20,6 @@ function validateUserPassword() {
     return false;
   } else {
     user_password.style.border = "2px solid #2da44e";
+    login_btn.style.opacity = "1";
   }
 }
