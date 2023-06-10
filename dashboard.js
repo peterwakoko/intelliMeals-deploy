2097c4b4894539d9318ffefe6d703f373cd31636
peterwakoko <peterwakoko@gmail.com>
2023-06-10 12:08:54 +0300

    new changes

--- dashboard.js
+++ dashboard.js
@@ -1,23 +1,41 @@
+console.log(window.matchMedia("(prefers-color-scheme: dark)").matches);
+
 const preloader = document.querySelector(".preloader");
 
 window.addEventListener("load", () => {
   preloader.style.display = "none";
 });
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-  if (!btn.classList.contains("slider")) {
-    btn.classList.add("slider");
-    document.body.style.background = "#000";
-    document.body.style.color = "#fff";
-  } else {
-    btn.classList.remove("slider");
-    document.body.style.background = "#fff";
-    document.body.style.color = "#000";
-  }
-});
+
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", (e) => {
+//   if (!btn.classList.contains("slider")) {
+//     btn.classList.add("slider");
+//     document.body.style.background = "#202124";
+//     document.body.style.color = "#fff";
+//     e.preventDefault();
+//   } else {
+//     btn.classList.remove("slider");
+//     document.body.style.background = "#fff";
+//     document.body.style.color = "#000";
+//   }
+// });
 
 const profile_image = document.querySelector(".profile-image");
 const profile_drop_down = document.querySelector(".profile-drop-down");
 profile_image.addEventListener("click", () => {
   profile_drop_down.style.display = "block";
 });
+
+function darkmode() {
+  const wasDarkmode = localStorage.getItem("darkmode") === "true";
+  localStorage.setItem("darkmode", !wasDarkmode);
+  const element = document.body;
+  element.classList.toggle("dark-mode", !wasDarkmode);
+}
+
+function onload() {
+  document.body.classList.toggle(
+    "dark-mode",
+    localStorage.getItem("darkmode") === "true"
+  );
+}
